23d5db7a4c10c013b8a26381af32d03afad4de8c
dalmatinos <dalmatinos@hotmail.com>
2026-04-06 15:07:17 +0200

    Fix comment formatting and typo in build.sh
    Corrected comment formatting and fixed a typo in the build script.

--- build_files/build.sh
+++ build_files/build.sh
@@ -21,9 +21,9 @@ set -ouex pipefail
 sudo dnf5 install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 sudo dnf5 install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
-#Install akmods og kernel headers
+# Install akmods og kernel headers
 dnf5 install -y akmods
-akmods --force --kernels 6.1
+
 
 # this installs a package from fedora and rpmfusion repos
 dnf5 install -y alacritty \
